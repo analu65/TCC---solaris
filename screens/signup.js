08a41b28f104e50660e15363f9a0d2ae8fadcf8d
analu65 <analuciacesario910@gmail.com>
2025-08-12 17:29:28 -0300

    hoje

---
+++ screens/signup.js
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View, Button, Image, TextInput } from "react-native";
+import { useState } from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../controller";
+
+export default function signUp({navigation}){
+    return (
+
+    );
+}
