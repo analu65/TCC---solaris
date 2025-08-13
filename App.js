f4d84ddce6c52228a457d3398207776be229cd8a
analu65 <analuciacesario910@gmail.com>
2025-08-13 13:54:12 -0300

    header e bottomtabs

--- App.js
+++ App.js
@@ -1,17 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Home from './screens/Home';
-import Avisos from './screens/Avisos';
+import Home from './screens/home';
+import Avisos from './screens/avisos';
 import { NavigationContainer } from '@react-navigation/native';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 
 export default function App() {
   const BottomTab = createBottomTabNavigator();
   return(
       <NavigationContainer>
-          <BottomTab.Navigator>
-              <BottomTab.Screen name='Home' component={Home}></BottomTab.Screen>
-              <BottomTab.Screen name='Avisos' component={Avisos}></BottomTab.Screen>
+          <BottomTab.Navigator screenOptions={{
+            animation: 'shift',
+            tabBarActiveBackgroundColor: '#d3d3d3',
+            headerShown: false
+          }}>
+              <BottomTab.Screen name='Home' component={Home} options={{tabBarIcon: () => (<MaterialCommunityIcons name="home" size={20} color="black" />)}}></BottomTab.Screen>
+              <BottomTab.Screen name='Avisos' component={Avisos} options={{tabBarIcon: () => (<MaterialCommunityIcons name="bell" size={20} color="black" />)}}></BottomTab.Screen>
           </BottomTab.Navigator>
       </NavigationContainer>
   )
